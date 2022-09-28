7b544da476322769d7910f857ecb687e47e0e8a7
DanielNasc <danielnasc15987@gmail.com>
2022-09-28 13:56:23 -0300

    arquivos independentes

--- menu.py
+++ menu.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 import re
 
 # diretorio onde os arquivos python e bash estão
@@ -11,16 +12,26 @@ def print_array(arr):
     for elem in arr:
         print(f"{i} => {elem}")
         i+=1
-        
+
+def choose_program():
+    print("\nDigite o numero do programa: ", end="")
+    return int(input())
+
 # =====================================================
 
 def main():
     # pega todos os arquivos dos exercicios (os que começam com ex e tem um numero depois)
     program_list = sorted(
-                            [ f for f in os.listdir(dir_path) if re.match(r'ex[0-9]', f) ], # array com todos os arquivos shell que começam com ex e têm um número depois
+                            [ f for f in os.listdir(dir_path) if re.match(r'ex_?[0-9]', f) ], # array com todos os arquivos shell que começam com ex e têm um número depois
                             key=lambda elem: int(re.sub('[^0-9]', '', elem)) # função que retorna o número do arquivo, usada para ordenar o arrays
                         ) # array ordenado
+
+    # [ PLACEHOLDERS | Luma troca depois pelas funções da interface ]
     print_array(program_list)
+    selected_number = choose_program()
+    selected_program = program_list[selected_number]
+
+    test = subprocess.call(f"./{selected_program}", shell=True)
 
 
 if __name__ == "__main__":
